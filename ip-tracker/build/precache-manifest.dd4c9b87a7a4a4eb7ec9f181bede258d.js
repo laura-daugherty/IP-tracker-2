f9f64734ab4f797bf8a93ff4cfae91b4a6c90fc3
Laura Daugherty <lauradaugherty6@gmail.com>
2020-11-11 11:20:04 -0800

    installing dependancies, creating build folder

---
+++ ip-tracker/build/precache-manifest.dd4c9b87a7a4a4eb7ec9f181bede258d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b73f17b068e9821a889e86c23ef2c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2256b14703f3096150ae",
+    "url": "/static/css/main.d01316b8.chunk.css"
+  },
+  {
+    "revision": "a8f0138637689389c6e2",
+    "url": "/static/js/2.3094fe50.chunk.js"
+  },
+  {
+    "revision": "f5846a532341dbf81a6fe52f73022fce",
+    "url": "/static/js/2.3094fe50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2256b14703f3096150ae",
+    "url": "/static/js/main.037ac3c3.chunk.js"
+  },
+  {
+    "revision": "76da375fcf6bbaee84cb",
+    "url": "/static/js/runtime-main.cfddd880.js"
+  },
+  {
+    "revision": "53ef8ed8ef4b92721f00cb8bfdc5c9b9",
+    "url": "/static/media/icon-arrow.53ef8ed8.svg"
+  },
+  {
+    "revision": "ff17146919959b508ee5dd9af2f9f9e5",
+    "url": "/static/media/icon-location.ff171469.svg"
+  },
+  {
+    "revision": "8d3f4ad64bf3a9320e172edf01e0c016",
+    "url": "/static/media/pattern-bg.8d3f4ad6.png"
+  }
+]);
